2862d1b4653a1f4e89fa542e661135a9b25660e9
Yaya Red <yaloveduff@gmail.com>
2016-03-15 14:50:53 +0800

    移除sticky 彈跳動畫，限定post 頁才跑 sticky 的 js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,7 +1,11 @@
 // Global Variables
 // ------------------------------------------------------
 
-$window = $(window);
+var $window = $(window);
+var breakpoints = {
+  pad: 640,
+  desktop: 960
+};
 
 function xx(a) {
   console.log(a);
@@ -20,7 +24,7 @@ var isMenuOpen = false;
 var toggleMenu = function() {
   $menuIcon.toggleClass('is-active', isMenuOpen);
   $offCanvasTarget.toggleClass('is-menu_open', isMenuOpen);
-}
+};
 
 var menu = {
   close: function() {
@@ -32,7 +36,7 @@ var menu = {
     isMenuOpen = !isMenuOpen;
     toggleMenu();
   }
-}
+};
 
 $menuSwitch.on('click', menu.toggle);
 $window.on('resize', menu.close);
@@ -43,55 +47,60 @@ $window.on('resize', menu.close);
 
 var $postShareToolbox = $('.js-post_share_toolbox');
 
-var postHeader = document.querySelectorAll('.js-post_header')[0];
-var authorBlock = document.querySelectorAll('.js-author_block')[0];
+(function(){
+  if (!$postShareToolbox.length) return;
 
-var postShareToolboxHeight = $postShareToolbox.height();
-var authorBlockHeight = authorBlock.offsetHeight;
+  var postHeader = document.querySelectorAll('.js-post_header')[0];
+  var authorBlock = document.querySelectorAll('.js-author_block')[0];
 
-var currentStatus = '';
+  var postShareToolboxHeight = $postShareToolbox.height();
+  var postShareToolboxMargin = parseInt($postShareToolbox.css('margin-top'), 10)
+  var authorBlockHeight = authorBlock.offsetHeight;
 
-var isMobile = function() {
-  return window.innerWidth < 640;
-}
+  var currentStatus = '';
 
-var getStatus = function () {
-  var status;
-  if (postHeader.getBoundingClientRect().bottom < 0) {
-    if (authorBlock.getBoundingClientRect().top < postShareToolboxHeight + 100) {
-      return status = 'bottom';
+  var isMobile = function() {
+    return window.innerWidth < breakpoints.pad;
+  };
+
+  var getStatus = function () {
+    var status;
+    if (postHeader.getBoundingClientRect().bottom - postShareToolboxMargin < 0) {
+      if (authorBlock.getBoundingClientRect().top < postShareToolboxHeight + postShareToolboxMargin) {
+        return status = 'bottom';
+      }
+      return status = 'sticky';
     }
-    return status = 'sticky';
-  }
-  return status = 'top';
-}
+    return status = 'top';
+  };
 
-var checkShareToolBoxPosition = function() {
-  if (isMobile()) return;
-  var status = getStatus();
+  var checkShareToolBoxPosition = function() {
+    if (isMobile()) {
+      $postShareToolbox[0].style.marginBottom = '';
+      return;
+    }
+    var status = getStatus();
 
-  if (currentStatus == status) return;
+    if (currentStatus == status) return;
 
-  currentStatus = status;
-  positionBottom = status == 'bottom' ? authorBlockHeight + 'px' : 0;
+    currentStatus = status;
+    marginBottom = status == 'bottom' ? authorBlockHeight + 'px' : 0;
 
-  $postShareToolbox
-    .removeClass('is-sticky is-bottom is-top')
-    .addClass('is-' + currentStatus)
-    .css('bottom', positionBottom);
-}
+    $postShareToolbox
+      .removeClass('is-sticky is-bottom is-top')
+      .addClass('is-' + currentStatus)
+      .css('margin-bottom', marginBottom);
+  };
 
-var updateAuthorBlockHeight = function() {
-  authorBlockHeight = authorBlock.offsetHeight;
-  currentStatus = '';
+  var updateAuthorBlockHeight = function() {
+    authorBlockHeight = authorBlock.offsetHeight;
+    currentStatus = '';
+    checkShareToolBoxPosition();
+  };
+
+
+  $window.on('resize', updateAuthorBlockHeight);
+  $window.on('scroll', checkShareToolBoxPosition);
+
   checkShareToolBoxPosition();
-}
-
-
-$window.on('resize', updateAuthorBlockHeight);
-$window.on('scroll', checkShareToolBoxPosition);
-
-checkShareToolBoxPosition();
-
-
-
+})()
